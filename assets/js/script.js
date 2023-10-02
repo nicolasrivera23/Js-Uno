dd853dbd9e0a032b65b1065b5dad5229730572d5
nicolasrivera23 <113543987+nicolasrivera23@users.noreply.github.com>
2023-10-02 15:37:05 -0300

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,33 @@
+  //constante
+  const PI = 3.1416;
 function preguntarNombre() {
   //declaramos una variable y le asignamos el valor que el usuario ingresa en el prompt
   let entrada = prompt('Ingresa tu nombre');
+
+  //no recomendado
+  variableSinTipo = 3;
+
 
   //invocamos al método/función alert que muestra el nombre almacenado en la variable
   alert('Bienvenido/a ' + entrada);
 
   //mostrar en consola el valor de la variable entrada
   console.log(entrada);
-
-  /* 
-  console.log(entrada);
-  console.log(entrada);
-  console.log(entrada);
-  console.log(entrada);
-  console.log(entrada);
-  */
+  console.log(variable);
 }
+
+function otraFuncion() {
+  let cadena = 'Hola Javascript';
+  let numero = 23;
+  let booleano = true;
+  let objeto = {
+    nombre : 'Nombre',
+    apellido : 'Apellido'
+  }
+
+
+  entrada2 = 33;
+  console.log(objeto);
+}
+
+let entrada2 = "Hoisting";
